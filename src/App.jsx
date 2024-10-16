a0e1a9cb9ca414974dd8ed605f02bf716a00dd5d
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-10-16 19:15:27 +0300

    start create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import Profile from "./components/Profile/Profile.jsx";
-import FriendList from "./components/FriendList/FriendList.jsx";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
-
 function App() {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "modern-normalize";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
